6ca90d18775359e3f0291ddd280e844445789d63
dannytheguy <danny@fusedmotion.com>
2017-12-09 15:10:30 -0500

    added barometric pressure

--- get.rb
+++ get.rb
@@ -9,23 +9,31 @@ def grab_data(device_name)
     
   dat = JSON.parse(cmd)
   temps = []
+  baros = []
 
   dat["objs"].each do |obj|
     time = obj["createDate"]["$date"]
     d = obj["data"]
     # puts JSON.pretty_generate(d)
     is_temp = false
+    is_baro = false
+    
     d.each do |measure|
       if is_temp == true
         temps.push([time,measure[1]])
         is_temp = false
+      elsif is_baro == true
+        baros.push([time,measure[1]])
+        is_baro = false
       end    
       if measure[1] == "Temperature Sensor"
         is_temp = true
+      elsif measure[1] == "Barometer"
+        is_baro = true
       end
     end  
   end
-  return { :name => device_name, :temps => temps[temps.length-1] }
+  return { :name => device_name, :temp => temps[temps.length-1], :baro => baros[baros.length-1] }
 end
 
 data_dan = grab_data('data_dan')
