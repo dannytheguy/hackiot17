c61e63decd9cc4e0834fd0174561704ddc252814
dannytheguy <danny@fusedmotion.com>
2017-12-09 13:35:27 -0500

    isolating temp

--- get.rb
+++ get.rb
@@ -1,18 +1,42 @@
 require 'rubygems'
 require 'json'
 
-
 cmd = `bash get.sh 2> /dev/null`
 
-# puts cmd
+hash = { :temps => [] }
 
 dat = JSON.parse(cmd)
 
 
-out = dat["objs"].each do |obj|
-  out = obj["data"]
-  puts JSON.pretty_generate(out)
+dat["objs"].each do |obj|
+  d = obj["data"]
+  # puts JSON.pretty_generate(d)
+  is_temp = false
+  temps = []
+  d.each do |measure|
+    # if measure == "Temperature Sensor"
+    #   puts "hit"
+    # end
+    # if measure[1] == "Temperature Sensor"
+    #   puts JSON.pretty_generate(measure)
+    # end
+    # puts JSON.pretty_generate(measure)
+    if is_temp == true
+      temps.push(measure[1])
+      # puts measure[1]
+      is_temp = false
+    end
+    
+    if measure[1] == "Temperature Sensor"
+      is_temp = true
+    end
+    
+  end
+  # puts JSON.pretty_generate(out)
+  puts temps
+  
 end
+
 
 # out = dat["objs"][0]["data"]
 
